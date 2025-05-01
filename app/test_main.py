2a3153f19f77d973278bcee3cc61dbdb8fdf94ab
Guysteeleperkins <guysteeleperkins@hotmail.com>
2025-05-01 18:18:01 +0100

    Added tests for set_page_config and set_page_title and passed tests with mocks

--- app/test_main.py
+++ app/test_main.py
@@ -1,35 +1,37 @@
 from unittest.mock import patch, MagicMock
 import pytest
-import main as main
+from app import main
 
 
 @pytest.fixture
 def mock_dependencies():
     """Fixture to mock all dependencies used in the main function."""
-    with patch("app.main.st.set_page_config") as mock_set_page_config, patch(
+    with patch("app.main.st.set_page_config"
+               ) as mock_set_page_config, patch(
         "app.main.st.title"
-    ) as mock_title, patch(
-        "app.main.load_and_clean_data"
-    ) as mock_load_and_clean_data, patch(
-        "app.main.apply_filters"
-    ) as mock_apply_filters, patch(
-        "app.main.display_metrics"
-    ) as mock_display_metrics, patch(
-        "app.main.display_visualizations"
-    ) as mock_display_visualizations:
+    ) as mock_title:
+    # patch(
+    #     "app.main.load_and_clean_data"
+    # ) as mock_load_and_clean_data, patch(
+    #     "app.main.apply_filters"
+    # ) as mock_apply_filters, patch(
+    #     "app.main.display_metrics"
+    # ) as mock_display_metrics, patch(
+    #     "app.main.display_visualizations"
+    # ) as mock_display_visualizations:
 
         # Set up mock return values
-        mock_load_and_clean_data.return_value = MagicMock()
-        mock_apply_filters.return_value = MagicMock()
+        # mock_load_and_clean_data.return_value = MagicMock()
+        # mock_apply_filters.return_value = MagicMock()
 
         # Yield all mocks for use in tests
         yield {
             "mock_set_page_config": mock_set_page_config,
             "mock_title": mock_title,
-            "mock_load_and_clean_data": mock_load_and_clean_data,
-            "mock_apply_filters": mock_apply_filters,
-            "mock_display_metrics": mock_display_metrics,
-            "mock_display_visualizations": mock_display_visualizations,
+            # "mock_load_and_clean_data": mock_load_and_clean_data,
+            # "mock_apply_filters": mock_apply_filters,
+            # "mock_display_metrics": mock_display_metrics,
+            # "mock_display_visualizations": mock_display_visualizations,
         }
   
 
