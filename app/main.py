3e00e5c371f034de78ff90b85ccfa8fbd3ae3312
Guysteeleperkins <guysteeleperkins@hotmail.com>
2025-05-01 17:59:24 +0100

    Added testing file with first test

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 import streamlit as st
 
+
 def main():
     """Main function to run Streamlit app."""
     st.set_page_config(
-        
+        page_title="GSP Garmin Data",
+        page_icon="🏃🏼‍♂️",
+        layout="wide",
+        initial_sidebar_state="auto",
     )
+
+    # Set the title of the app
+    st.title("GSP Garmin Dataset Explorer")
